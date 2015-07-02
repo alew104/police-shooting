c4bdbe8bbc31f5248f611ac3180644ec9003e14b
Alex Lew <alew104@gmail.com>
2015-07-02 16:03:06 -0700

    ajax call completed
    eom

--- js/map.js
+++ js/map.js
@@ -1,20 +1,35 @@
 // Function to draw your map
 var drawMap() = function() {
+  var map = L.map('container');
+  map.setView([34,-100],10);
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+  layer.addTo(map);
 
   // Create map and set viewd
- 
+
 
   // Create an tile layer variable using the appropriate url
 
   // Add the layer to your map
- 
+
 
   // Execute your function to get data
- 
+
 }
 
 // Function for getting data
 var getData = function() {
+  var data;
+    $.ajax({
+    url:'https://data.seattle.gov/resource/7ais-f98f.json?year=2015&$limit=500',
+    type: "get",
+    success:function(dat) {
+       data = dat
+    },
+   dataType:"json"
+})
+
+customBuild(data);
 
   // Execute an AJAX request to get the data in data/response.js
 
@@ -23,10 +38,16 @@ var getData = function() {
 
 }
 
-// Do something creative with the data here!  
-var customBuild = function() {
-
-  
+// Do something creative with the data here!
+var customBuild = function(data)
+  //grab all the data
+  for (var i = 0; i < data.length; i++){
+      var offense_type = data.offense_type;
+      var data_reported = data.data_reported;
+      var latitude = data.location.latitude;
+      var longitude = data.location.longitude;
+      var month = data.month;
+      var hundred_block_location = data.hundred_block_location;
+      var summarized_offense_description = data.summarized_offense_description;
+  }
 }
-
-
